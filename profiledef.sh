264f61763172ac7e076edd07f3376a4114562bd7
Panda Foss <maxi.fg13@gmail.com>
2021-11-27 21:57:43 -0300

    Bump v1.3.4 version

--- profiledef.sh
+++ profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="anarchy"
-iso_label="ANARCHY134beta1"
+iso_label="ANARCHY134"
 iso_publisher="Anarchy Installer <https://anarchyinstaller.gitlab.io>"
 iso_application="Anarchy Installer"
-iso_version="1.3.4.beta1"
+iso_version="1.3.4"
 install_dir="anarchy"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
